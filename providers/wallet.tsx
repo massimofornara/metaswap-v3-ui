56d0fd24cf4649d8a8df4068e88e5739c2265a0d
massimofornara <mfornara93@gmail.com>
2026-04-25 03:25:00 +0200

    Stabilized dependencies and fixed wagmi config

--- providers/wallet.tsx
+++ providers/wallet.tsx
@@ -4,10 +4,12 @@ import { WagmiProvider } from "wagmi";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { wagmiConfig } from "@/wagmiConfig";
 
-export function Web3Provider({ children }: { children: React.ReactNode }) {
+export function Web3Provider({ children }) {
   return (
     <WagmiProvider config={wagmiConfig}>
-      <RainbowKitProvider>{children}</RainbowKitProvider>
+      <RainbowKitProvider>
+        {children}
+      </RainbowKitProvider>
     </WagmiProvider>
   );
 }
